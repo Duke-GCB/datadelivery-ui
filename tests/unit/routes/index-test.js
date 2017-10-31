67619756e7c47a906b1e89ef8ccde5ad85a7b254
Dan Leehr <dan.leehr@duke.edu>
2017-10-31 16:32:13 -0400

    Test for redirect on index route

--- tests/unit/routes/index-test.js
+++ tests/unit/routes/index-test.js
@@ -1,11 +1,18 @@
 import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('route:index', 'Unit | Route | index', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
 });
 
 test('it exists', function(assert) {
   let route = this.subject();
   assert.ok(route);
+});
+
+test('it redirects to deliveries', function(assert) {
+  let route = this.subject({
+    replaceWith(routeName) {
+      assert.equal(routeName, 'deliveries','replace with route name deliveries');
+    }
+  });
+  route.beforeModel();
 });
