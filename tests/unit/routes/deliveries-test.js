b08bc2bb8d62ff31c89a868dc3e107b723e5df6d
Dan Leehr <dan.leehr@duke.edu>
2017-11-01 10:56:01 -0400

    Tests for deliveries route
    - Tests that the model fetches all deliveries from store

--- tests/unit/routes/deliveries-test.js
+++ tests/unit/routes/deliveries-test.js
@@ -1,10 +1,29 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleFor('route:deliveries', 'Unit | Route | deliveries', {
-  needs: ['service:session']
+  needs: ['service:session', 'model:delivery']
 });
 
 test('it exists', function(assert) {
   let route = this.subject();
   assert.ok(route);
+});
+
+test('it fetches all deliveries', function(assert) {
+  assert.expect(2);
+  const mockDeliveries = [
+    Ember.Object.create({id: 1}),
+    Ember.Object.create({id: 2})
+  ];
+  const MockStore = Ember.Service.create({
+    findAll(model) {
+      assert.equal(model, 'delivery');
+      return mockDeliveries;
+    }
+  });
+
+  let route = this.subject({store: MockStore});
+  const deliveries = route.model();
+    assert.deepEqual(deliveries, mockDeliveries);
 });
