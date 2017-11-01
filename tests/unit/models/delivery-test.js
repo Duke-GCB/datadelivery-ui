35b926c3494e7fa6654015fa3c12ed1b2a688613
Dan Leehr <dan.leehr@duke.edu>
2017-11-01 10:44:46 -0400

    Tests relationships from delivery

--- tests/unit/models/delivery-test.js
+++ tests/unit/models/delivery-test.js
@@ -1,4 +1,5 @@
 import { moduleForModel, test } from 'ember-qunit';
+import { testRelationships } from '../../helpers/test-relationships';
 
 moduleForModel('delivery', 'Unit | Model | delivery', {
   // Specify the other units that are required for this test.
@@ -10,3 +11,11 @@ test('it exists', function(assert) {
   // let store = this.store();
   assert.ok(!!model);
 });
+
+const relationships = [
+  {key: 'project', kind: 'belongsTo', type: 'duke-ds-project'},
+  {key: 'fromUser', kind: 'belongsTo', type: 'duke-ds-user'},
+  {key: 'toUser', kind: 'belongsTo', type: 'duke-ds-user'}
+];
+
+testRelationships('delivery', relationships);
