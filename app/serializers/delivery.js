d5c5ca2300e105f54d1f8e2bed6eba68376560cb
Dan Leehr <dan.leehr@duke.edu>
2018-01-30 15:47:15 -0500

    Delivery serializer handles hasMany relationships
    E.g. `shareUsers` to `share_user_ids`

--- app/serializers/delivery.js
+++ app/serializers/delivery.js
@@ -1,11 +1,20 @@
 import ApplicationSerializer from './application';
+import { singularize } from 'ember-inflector';
 
 export default ApplicationSerializer.extend({
   // Related fields from the delivery (e.g. project) are specified with an `_id` suffix in the
   // incoming JSON payload. But Ember treats these as related objects, so it's more natural for
   // the ember model to use a field like `project` rather than `project_id`
-  keyForRelationship(key) {
+  keyForRelationship(key, relationship) {
     const localKey = this._super(key);
-    return `${localKey}_id`;
+    if(relationship == 'hasMany') {
+      // For hasMany relationships, make the key singular, since we'll be pluralizing id
+      return `${singularize(localKey)}_ids`;
+    } else if(relationship == 'belongsTo') {
+      // for belongsTo relationships, just append _id
+      return `${localKey}_id`;
+    } else {
+      return localKey;
+    }
   }
 });
--- tests/unit/serializers/delivery-test.js
+++ tests/unit/serializers/delivery-test.js
@@ -2,10 +2,20 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('serializer:delivery', 'Unit | Serializer | delivery');
 
-test('it converts relationship keys to underscore and appends _id', function (assert) {
+test('it converts belongsTo relationships to underscore and appends _id', function (assert) {
   let serializer = this.subject();
-  const relationshipKey = serializer.keyForRelationship('someRelationship');
-  assert.equal(relationshipKey, 'some_relationship_id');
-  const attributeKey = serializer.keyForAttribute('someAttribute');
-  assert.equal(attributeKey, 'some_attribute');
+  const belongsToKey = serializer.keyForRelationship('fromUser', 'belongsTo');
+  assert.equal(belongsToKey, 'from_user_id');
+});
+
+test('it converts hasMany relationships to underscore, singularizes, and appends _ids', function (assert) {
+  let serializer = this.subject();
+  const hasMany = serializer.keyForRelationship('shareUsers', 'hasMany');
+  assert.equal(hasMany, 'share_user_ids');
+});
+
+test('it does not append anything to attributes', function (assert) {
+  let serializer = this.subject();
+  const attributeKey = serializer.keyForAttribute('userMessage');
+  assert.equal(attributeKey, 'user_message');
 });
