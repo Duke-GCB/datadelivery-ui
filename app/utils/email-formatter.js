5ac71245d40bd3ab239f2a2102b9c1e2022cdd57
John Bradley <johnbradley2008@gmail.com>
2018-02-27 15:43:56 -0500

    break email text formatting out into util
    adds email-formatter util so it is easier unit test email formatting.

---
+++ app/utils/email-formatter.js
@@ -0,0 +1,63 @@
+const HEADER_FIELD_SEPERATOR = ':';
+
+function getEmailHeaderAndBody(value) {
+  /**
+   * Separates body and header in email value (https://tools.ietf.org/html/rfc7230#section-3)
+   *returns a object containing the body as str and header as a dictionary
+   */
+  var header = {};
+  var body = '';
+  var inBody = false;
+  value.split('\n').forEach(function (line) {
+    if (!inBody && line.trim() == '') { // first CRLF by itself
+      inBody = true;
+    } else {
+      if (inBody) {
+        body += line + '\n';
+      } else {
+        const sepIdx = line.indexOf(HEADER_FIELD_SEPERATOR);
+        if (sepIdx > -1) {
+          const fieldName = line.substr(0, sepIdx);
+          const fieldValue = line.substr(sepIdx + 1);
+
+          header[fieldName] = fieldValue;
+        }
+      }
+    }
+  });
+  return {
+    header: header,
+    body: body
+  };
+}
+
+
+function getOptionalHeaderLine(headerDict, name) {
+  /**
+   * Returns
+   */
+  var value = headerDict[name];
+  if (value) {
+    return name + HEADER_FIELD_SEPERATOR + value + '\n';
+  }
+  return '';
+}
+
+
+function formatEmailText(value) {
+  /**
+   * Strips header from email text with the exception of Subject, From, To and Date
+   */
+  const headerFields = ['Subject', 'From', 'To', 'Date'];
+  var headerAndBody = getEmailHeaderAndBody(value);
+  var result = '';
+  headerFields.forEach(function (headerFieldName) {
+    result += getOptionalHeaderLine(headerAndBody.header, headerFieldName);
+  });
+  result += '\n';
+  result += headerAndBody.body;
+  return result;
+}
+
+
+export {formatEmailText, getEmailHeaderAndBody, getOptionalHeaderLine}
---
+++ tests/unit/utils/email-formatter-test.js
@@ -0,0 +1,58 @@
+import {formatEmailText, getEmailHeaderAndBody, getOptionalHeaderLine} from 'datadelivery-ui/utils/email-formatter';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | email formatter');
+
+// Replace this with your real tests.
+test('formatEmailText can format a simple email', function(assert) {
+  var value = 'To: joe@joe.joe\n\nEmail Body'
+  let result = formatEmailText(value);
+  assert.equal(result.trim(), `To: joe@joe.joe
+
+Email Body`);
+});
+
+test('formatEmailText can pull out all necessary header fields and filter out other header fields', function(assert) {
+  var value = `To: joe@joe.joe
+From: bob@bob.bob
+Subject: Learn Something
+HeaderJunk: Yep
+Date: Jan 2 2018 12:45
+MessageID: 123909123091233
+
+Email Body
+More Email Body`;
+  let result = formatEmailText(value);
+  assert.equal(result.trim(), `Subject: Learn Something
+From: bob@bob.bob
+To: joe@joe.joe
+Date: Jan 2 2018 12:45
+
+Email Body
+More Email Body`);
+});
+
+test('getEmailHeaderAndBody can create dictionary from header lines and str from body', function(assert) {
+  const result = getEmailHeaderAndBody(`From: Joe
+
+bodyData
+AnotherLine`);
+
+  assert.equal(result.header.From.trim(), 'Joe')
+  assert.equal(result.body.trim(), 'bodyData\nAnotherLine')
+});
+
+test('getOptionalHeaderLine returns empty string for items not in headerDict', function(assert) {
+  const headerDict = {
+    From: 'joe@joe.joe'
+  };
+  assert.equal(getOptionalHeaderLine(headerDict, 'Subject'), '');
+});
+
+test('getOptionalHeaderLine returns field and value joined together for items in headerDict', function(assert) {
+  const headerDict = {
+    From: ' joe@joe.joe'
+  };
+  assert.equal(getOptionalHeaderLine(headerDict, 'From'), 'From: joe@joe.joe\n');
+
+});
