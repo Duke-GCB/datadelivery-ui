493ed00b399592effe23cd26b9a919d062f8e92b
John Bradley <johnbradley2008@gmail.com>
2018-03-22 10:52:07 -0400

    tests for deliveries show and show/index routes

--- tests/unit/routes/deliveries/show-test.js
+++ tests/unit/routes/deliveries/show-test.js
@@ -1,11 +1,24 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleFor('route:deliveries/show', 'Unit | Route | deliveries/show', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
 
-test('it exists', function(assert) {
-  let route = this.subject();
-  assert.ok(route);
+test('it finds a delivery based on delivery_id', function(assert) {
+  let route = this.subject({
+    store: {
+      findRecord(recordModel, id) {
+        return Ember.RSVP.resolve(Ember.Object.create({id: id, kind: 'find_' + recordModel}));
+      }
+    }
+  });
+
+  Ember.run(() => {
+    route.model({delivery_id: 1}).then(model => {
+      assert.equal(model.get('kind'), 'find_delivery');
+      assert.equal(model.get('id'), 1);
+    });
+  });
 });
--- tests/unit/routes/deliveries/show/index-test.js
+++ tests/unit/routes/deliveries/show/index-test.js
@@ -1,11 +1,18 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleFor('route:deliveries/show/index', 'Unit | Route | deliveries/show/index', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
 
-test('it exists', function(assert) {
-  let route = this.subject();
-  assert.ok(route);
+test('it resets infoMessage when exiting', function(assert) {
+  let route = this.subject({});
+  let controller = Ember.Object.create({
+    infoMessage: 'Warning'
+  });
+  route.resetController(controller, false);
+  assert.equal(controller.infoMessage, 'Warning');
+  route.resetController(controller, true);
+  assert.equal(controller.infoMessage, null);
 });
