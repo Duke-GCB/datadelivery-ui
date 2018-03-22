bce91bee3767b396cd291f189e0cc4a9e38f4bf0
John Bradley <johnbradley2008@gmail.com>
2018-03-22 10:20:55 -0400

    test resend controller
    transitions to deliveries.show and displays a message
    on successful resend

--- tests/unit/controllers/deliveries/show/resend-test.js
+++ tests/unit/controllers/deliveries/show/resend-test.js
@@ -1,12 +1,39 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from "ember";
 
 moduleFor('controller:deliveries/show/resend', 'Unit | Controller | deliveries/show/resend', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  let controller = this.subject();
-  assert.ok(controller);
+test('it resending delivery transitions to deliveris.show', function(assert) {
+  let done = assert.async();
+  const delivery = Ember.Object.create({
+    transfer: {
+      project: {
+        name: 'mouse'
+      }
+    },
+  });
+
+  delivery.save = function () {
+      return Ember.RSVP.resolve(delivery);
+  };
+
+  delivery.send = function (force) {
+      assert.equal(force, true);
+      return Ember.RSVP.resolve(delivery);
+  };
+
+  let controller = this.subject({
+    model: delivery,
+    transitionToRoute(routeName, object, options) {
+      assert.equal(routeName, 'deliveries.show');
+      assert.equal(object, delivery);
+      assert.equal(options.queryParams.infoMessage, 'Email message resent for delivery of project mouse.');
+      done();
+    }
+  });
+  controller.send('resend');
+  assert.equal(1, 1);
 });
