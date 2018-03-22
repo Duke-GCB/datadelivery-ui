165173d98b0758d9132d1af06429e05631e9934d
John Bradley <johnbradley2008@gmail.com>
2018-03-22 09:57:18 -0400

    test deliveries controller computed property

--- tests/unit/controllers/deliveries/show/index-test.js
+++ tests/unit/controllers/deliveries/show/index-test.js
@@ -1,12 +1,25 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleFor('controller:deliveries/show/index', 'Unit | Controller | deliveries/show/index', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
 });
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  let controller = this.subject();
-  assert.ok(controller);
+test('it computes showResend true', function(assert) {
+  let controller = this.subject({
+    model: Ember.Object.create({
+      canResend: true
+    })
+  });
+  assert.equal(controller.get('showResend'), true);
+});
+
+test('it computes showResend false', function(assert) {
+  let controller = this.subject({
+    model: Ember.Object.create({
+      canResend: false
+    })
+  });
+  assert.equal(controller.get('showResend'), false);
 });
