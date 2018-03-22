6d9f10d731d62e495a1e4bd45d3de12ad8e02aee
John Bradley <johnbradley2008@gmail.com>
2018-03-22 09:25:49 -0400

    simplify deliveries/show index and resend
    moves model creation to top level route show.js.
    The show/index and show/resend routes inherit this
    code even though they extend Ember.Route.

--- app/routes/deliveries/show/resend.js
+++ app/routes/deliveries/show/resend.js
@@ -1,8 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    let params = this.paramsFor('deliveries.show');
-    return this.get('store').findRecord('delivery', params.delivery_id);
-  }
 });
