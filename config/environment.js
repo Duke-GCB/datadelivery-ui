2eaa383fbb7953fe8deee13376f9b00e340ea582
Dan Leehr <dan.leehr@duke.edu>
2018-06-06 16:24:34 -0400

    Fill in missing customizations for djangorestframework-jwt
    - refreshToken property name is 'token'
    - authorization prefix in header is 'JWT '

--- config/environment.js
+++ config/environment.js
@@ -51,8 +51,13 @@ module.exports = function(environment) {
   }
 
   ENV['ember-simple-auth-token'] = {
+    // refreshAccessTokens: true,
+    // refreshLeeway: 300, // refresh 5 minutes (300 seconds) before expiration
     serverTokenEndpoint: ENV.APP.API_URL + '/auth/api-token-auth/', // Server endpoint to send authenticate request
-    serverTokenRefreshEndpoint: ENV.APP.API_URL + '/auth/api-token-refresh/'
+    serverTokenRefreshEndpoint: ENV.APP.API_URL + '/auth/api-token-refresh/',
+    tokenPropertyName: 'token', // Key in server response that contains the access token
+    refreshTokenPropertyName: 'token', // Key in server response that contains the refresh token. Same as the access token.
+    authorizationPrefix: 'JWT '
   };
 
   return ENV;
