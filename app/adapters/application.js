ac347920ad626844dd042fa02df6c7b8a476939f
Dan Leehr <dan.leehr@duke.edu>
2018-06-06 11:38:42 -0400

    ApplicationAdapter simplification
    Token-specific behavior is now in drf-ember-frontend

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,7 @@
 import DRFAdapter from 'drf-ember-frontend/adapters/drf-adapter';
 import ENV from 'datadelivery-ui/config/environment'; // This is how we load config variables from our environment.js file
-import TokenAuthorizerMixin from 'ember-simple-auth-token/mixins/token-authorizer';
 
-export default DRFAdapter.extend(TokenAuthorizerMixin, {
+export default DRFAdapter.extend({
   host: ENV.APP.API_URL,
   namespace: ENV.APP.API_NAMESPACE
 });
