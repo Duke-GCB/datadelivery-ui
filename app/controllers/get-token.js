bc3a5285fe756f8e7e5b0d6d4650c769e7d8d92f
Dan Leehr <dan.leehr@duke.edu>
2018-06-07 14:40:59 -0400

    get-token controller that tries to authenticate
    - Currently a WIP, and no tests yet

---
+++ app/controllers/get-token.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  session: Ember.inject.service('session'),
+  init() {
+    this.getToken();
+  },
+  getToken() {
+    const authenticator = 'authenticator:jwt-session-authenticator'; // Find the authenticator that uses the session
+    const credentials = {};
+    const success = () => {
+      this.transitionToRoute('/deliveries');
+    };
+    const failure = (reason) => {
+      this.set('errorMessage', reason);
+      // this.transitionToRoute('/login');
+    };
+    this.get('session')
+      .authenticate(authenticator, credentials)
+      .then(success, failure);
+  }
+});
