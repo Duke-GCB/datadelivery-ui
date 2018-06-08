a93c5bc008e16d173ea0a947326a6cafd2ebfb0e
Dan Leehr <dan.leehr@duke.edu>
2018-06-08 15:02:31 -0400

    Using LoginController from drf-ember-frontend

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,18 +1,3 @@
-import Ember from 'ember';
-import ENV from 'datadelivery-ui/config/environment';
+import LoginController from 'drf-ember-frontend/controllers/login';
 
-export default Ember.Controller.extend({
-  session: Ember.inject.service('session'),
-  isDevelopmentMode: ENV.environment === 'development',
-  authorizeUrl: ENV.APP.AUTHORIZE_URL,
-  actions: {
-    authenticate() {
-      // Now using JWT
-      const authenticator = 'authenticator:jwt-authenticator'; // Causes JWT authenticator to be used
-      const credentials = this.getProperties('username', 'password');
-      this.get('session').authenticate(authenticator, credentials).catch((reason) => {
-        this.set('errorMessage', reason);
-      });
-    }
-  }
-});
+export default LoginController.extend();
