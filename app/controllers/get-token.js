c3c4593f39f4017fbd36a1b31f3104a9e1ec5a77
Dan Leehr <dan.leehr@duke.edu>
2018-06-08 14:16:39 -0400

    Moved get-token functionality to drf-ember-frontend

--- app/controllers/get-token.js
+++ app/controllers/get-token.js
@@ -1,25 +1,6 @@
-import Ember from 'ember';
+import GetTokenController from 'drf-ember-frontend/controllers/get-token';
 
-export default Ember.Controller.extend({
-  session: Ember.inject.service('session'),
-  init() {
-    this.getToken();
-  },
-  getToken() {
-    const authenticator = 'authenticator:jwt-session-authenticator'; // Find the authenticator that uses the session
-    const credentials = {};
-    const success = () => {
-      this.transitionToRoute('/deliveries');
-    };
-    const failure = (/* reason */) => {
-      this.transitionToRoute('/login');
-    };
-    const session = this.get('session');
-
-    if(session.get('isAuthenticated')) {
-      success();
-    } else {
-        session.authenticate(authenticator, credentials).then(success, failure);
-    }
-  }
+export default GetTokenController.extend({
+  successRoute: '/deliveries',
+  failureRoute: '/login',
 });
--- tests/unit/controllers/get-token-test.js
+++ tests/unit/controllers/get-token-test.js
@@ -1,12 +1,12 @@
 import { moduleFor, test } from 'ember-qunit';
-import Ember from 'ember';
-import { resolve, reject } from 'rsvp';
+import EmberObject from '@ember/object';
+import { resolve } from 'rsvp';
 
 moduleFor('controller:get-token', 'Unit | Controller | get token', {
   needs: ['service:session'],
 });
 
-const MockSession = Ember.Object.extend({
+const MockSession = EmberObject.extend({
   isAuthenticated: false,
   authenticate() { return resolve(); }
 });
@@ -17,56 +17,6 @@ test('it exists', function(assert) {
     transitionToRoute() {}
   });
   assert.ok(controller);
-});
-
-test('it authenticates with jwt-session-authenticator when not authenticated', function(assert) {
-  assert.expect(1);
-  const session = MockSession.create({
-    authenticate(authenticator) {
-      assert.equal(authenticator, 'authenticator:jwt-session-authenticator');
-      return resolve();
-    },
-  });
-  this.subject({
-    session: session,
-    transitionToRoute() {}
-  });
-});
-
-test('it does not call authenticate() when session is authenticated', function(assert) {
-  assert.expect(1);
-  const session = MockSession.create({
-    isAuthenticated: true,
-    authenticate() { assert.fail(); },
-  });
-  this.subject({
-    session: session,
-    transitionToRoute() { assert.ok(true); }
-  });
-});
-
-test('it transitions to /login when authentication fails', function(assert) {
-  assert.expect(1);
-  const session = MockSession.create({
-    authenticate() { return reject(); }
-  });
-  this.subject({
-    session: session,
-    transitionToRoute(destination) {
-      assert.equal(destination, '/login');
-    }
-  });
-});
-
-test('it transitions to /deliveries when authentication succeeds', function(assert) {
-  assert.expect(1);
-  const session = MockSession.create({
-    authenticate() { return resolve(); },
-  });
-  this.subject({
-    session: session,
-    transitionToRoute(destination) {
-      assert.equal(destination, '/deliveries');
-    }
-  });
+  assert.equal(controller.get('successRoute'), '/deliveries');
+  assert.equal(controller.get('failureRoute'), '/login');
 });
