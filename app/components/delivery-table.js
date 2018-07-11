4ad24f29217539300ac09dc619ac5d491798bdf2
John Bradley <johnbradley2008@gmail.com>
2018-07-11 14:50:44 -0400

    simplify delivery-table columns declaration

--- app/components/delivery-table.js
+++ app/components/delivery-table.js
@@ -3,10 +3,10 @@ import Ember from 'ember';
 const DeliveryTable = Ember.Component.extend({
   tagName: 'div',
   columns: [
-    {"propertyName": "project.name" ,"title": "Project Name", "routeName": "deliveries.show"},
-    {"propertyName": "fromUser.fullName" ,"title": "From"},
-    {"propertyName": "toUsersNames" ,"title": "To"},
-    {"propertyName": "status" ,"title": "State"}
+    { propertyName: "project.name", title: "Project Name", routeName: "deliveries.show"},
+    { propertyName: "fromUser.fullName", title: "From"},
+    { propertyName: "toUsersNames", title: "To"},
+    { propertyName: "status", title: "State"}
   ]
 });
 
