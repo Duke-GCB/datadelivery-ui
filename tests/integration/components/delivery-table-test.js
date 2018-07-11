5783bc5ac0e1c67ed1a23fbcc8ed7b53561089e4
John Bradley <johnbradley2008@gmail.com>
2018-07-11 13:18:34 -0400

    incorporate testing logic from removed tests

--- tests/integration/components/delivery-table-test.js
+++ tests/integration/components/delivery-table-test.js
@@ -5,8 +5,31 @@ moduleForComponent('delivery-table', 'Integration | Component | delivery table',
   integration: true
 });
 
+function removeMultipleSpacesAndNewlines(str) {
+  return str.replace(/ +/g, ' ').replace(/\n/g, '');
+}
+
 test('it renders', function(assert) {
-  const transfers = [{id: 1}, {id: 2}, {id: 3}];
+  const transfers = [{
+    id: 3,
+    project: {name: 'Taco'},
+    fromUser: {fullName: 'Arthur Adamson'},
+    toUsersNames: 'Zelda Zellington',
+    status: 'pending'
+  }, {
+    id: 2,
+    project: {name: 'Burger'},
+    fromUser: {fullName: 'Zelda Zellington'},
+    toUsersNames: 'Arthur Adamson',
+    status: 'accepted'
+  }, {
+    id: 3,
+    project: {name: 'Pizza'},
+    fromUser: {fullName: 'Arthur Adamson'},
+    toUsersNames: 'Zelda Zellington',
+    status: 'rejected'
+
+  }];
   this.set('transfers', transfers);
 
   this.render(hbs`{{delivery-table transfers}}`);
@@ -16,6 +39,15 @@ test('it renders', function(assert) {
   assert.equal(this.$('tbody tr a').length, 3); // each row should be selectable
 
   // displays expected column names
-  const columnNames = this.$('thead tr th').text().trim().replace(/ +/g, ' ').replace(/\n/g, '');
+  const columnNames = removeMultipleSpacesAndNewlines(this.$('thead tr th').text().trim());
   assert.equal(columnNames, 'Project Name  From  To  State');
+
+  const firstField = removeMultipleSpacesAndNewlines(this.$('tbody tr td').eq(0).text().trim());
+  assert.equal(firstField, 'Taco');
+  const secondField = removeMultipleSpacesAndNewlines(this.$('tbody tr td').eq(1).text().trim());
+  assert.equal(secondField, 'Arthur Adamson');
+  const thirdField = removeMultipleSpacesAndNewlines(this.$('tbody tr td').eq(2).text().trim());
+  assert.equal(thirdField, 'Zelda Zellington');
+  const fourthField = removeMultipleSpacesAndNewlines(this.$('tbody tr td').eq(3).text().trim());
+  assert.equal(fourthField, 'pending');
 });
