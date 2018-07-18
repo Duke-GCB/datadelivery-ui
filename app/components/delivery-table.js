c4f7dcdd35ea8c3c33393d0dd60ac9738c19b8a4
Dan Leehr <dan.leehr@duke.edu>
2018-07-18 16:09:40 -0400

    Splits deliveries table into outgoing and incoming

--- app/components/delivery-table.js
+++ app/components/delivery-table.js
@@ -1,17 +1,33 @@
 import Ember from 'ember';
 
 const DeliveryTable = Ember.Component.extend({
+  currentDukeDsUser: null,
   tagName: 'div',
-  columns: [
+  outgoingTransfers: Ember.computed('transfers.[]', 'currentDukeDsUser', function() {
+    const currentDukeDsUserId = this.get('currentDukeDsUser.id');
+    return this.get('transfers').filterBy('fromUser.id', currentDukeDsUserId);
+  }),
+  incomingTransfers: Ember.computed('transfers.[]', 'currentDukeDsUser', function() {
+    const currentDukeDsUserId = this.get('currentDukeDsUser.id');
+    return this.get('transfers').filter(function(transfer) {
+      return transfer.get('toUsers').mapBy('id').includes(currentDukeDsUserId);
+    });
+  }),
+  outgoingColumns: [
     { propertyName: "project.name", title: "Project Name", routeName: "deliveries.show"},
-    { propertyName: "fromUser.fullName", title: "From"},
     { propertyName: "toUsersNames", title: "To"},
     { propertyName: "status", title: "State"}
+  ],
+  incomingColumns: [
+    { propertyName: "project.name", title: "Project Name", routeName: "deliveries.show"},
+    { propertyName: "fromUser.fullName", title: "From"},
+    { propertyName: "status", title: "State"}
   ]
+
 });
 
 DeliveryTable.reopenClass({
-  positionalParams: ['transfers']
+  positionalParams: ['transfers', 'currentDukeDsUser']
 });
 
 export default DeliveryTable;
