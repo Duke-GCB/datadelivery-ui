466b672dbb095159ab1274d3a73f44dd6067f6f0
John Bradley <johnbradley2008@gmail.com>
2018-07-26 10:56:50 -0400

    remove unused model

--- app/models/delivery-answer-set.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  project: DS.belongsTo('DukeDsProject'),
-  fromUser: DS.belongsTo('DukeDsUser'),
-  toUser: DS.belongsTo('DukeDsUser'),
-  shareUsers: DS.hasMany('DukeDsUser'),
-  userMessage: DS.attr('string'),
-});
