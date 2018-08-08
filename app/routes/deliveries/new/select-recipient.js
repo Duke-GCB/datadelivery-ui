2871f016af92920b8fc3f34c10216ba67cf8b000
John Bradley <johnbradley2008@gmail.com>
2018-08-08 16:54:06 -0400

    force reload of recipient list

--- app/routes/deliveries/new/select-recipient.js
+++ app/routes/deliveries/new/select-recipient.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.get('store').findAll('duke-ds-user');
+    // force loading all list of users so they don't start filling in after a few seconds
+    return this.get('store').findAll('duke-ds-user', {reload: true});
   }
 });
