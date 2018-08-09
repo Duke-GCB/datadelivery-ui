b4934772a9c104ef2a734ae522e4dc024ab871e0
John Bradley <johnbradley2008@gmail.com>
2018-08-09 13:38:25 -0400

    test sorting by user Name

--- tests/integration/components/duke-ds-user-list-test.js
+++ tests/integration/components/duke-ds-user-list-test.js
@@ -5,21 +5,22 @@ moduleForComponent('duke-ds-user-list', 'Integration | Component | duke ds user 
   integration: true
 });
 
-test('it renders a list of users', function(assert) {
+test('it renders a list of users sorted by fullName', function(assert) {
   this.set('users', [
-    {fullName: 'Joe', email: 'joe@joe.org'},
-    {fullName: 'Jim', email: 'jim@jim.org'},
+    {id:'1', fullName: 'Joe', email: 'joe@joe.org'},
+    {id:'2', fullName: 'Jim', email: 'jim@jim.org'},
   ]);
+  this.set('externalAction', function () {});
+  this.render(hbs`{{duke-ds-user-list users selectionChanged=(action externalAction)}}`);
 
-  this.render(hbs`{{duke-ds-user-list users}}`);
   //assert.equal(this.$().html().trim(), 'Project Name');
   assert.equal(this.$('.duke-ds-user-fullName').eq(0).text().trim(), 'Name'); //header
   assert.equal(this.$('.duke-ds-user-email').eq(0).text().trim(), 'Email'); //header
   // index 1 is the search box
-  assert.equal(this.$('.duke-ds-user-fullName').eq(2).text().trim(), 'Joe');
-  assert.equal(this.$('.duke-ds-user-email').eq(2).text().trim(), 'joe@joe.org');
-  assert.equal(this.$('.duke-ds-user-fullName').eq(3).text().trim(), 'Jim');
-  assert.equal(this.$('.duke-ds-user-email').eq(3).text().trim(), 'jim@jim.org');
+  assert.equal(this.$('.duke-ds-user-fullName').eq(2).text().trim(), 'Jim');
+  assert.equal(this.$('.duke-ds-user-email').eq(2).text().trim(), 'jim@jim.org');
+  assert.equal(this.$('.duke-ds-user-fullName').eq(3).text().trim(), 'Joe');
+  assert.equal(this.$('.duke-ds-user-email').eq(3).text().trim(), 'joe@joe.org');
 });
 
 test('it sends selected project to selectionChanged action', function(assert) {
@@ -28,7 +29,11 @@ test('it sends selected project to selectionChanged action', function(assert) {
     {fullName: 'Joe', email: 'joe@joe.org'},
     {fullName: 'Jim', email: 'jim@jim.org'},
   ]);
-  this.set('externalAction', (actionData) => assert.equal(actionData.selectedItems[0].fullName, 'Jim'));
+  this.set('externalAction', function (actionData) {
+    if (actionData.selectedItems[0]) {
+      assert.equal(actionData.selectedItems[0].fullName, 'Jim');
+    }
+  });
   this.render(hbs`{{duke-ds-user-list users selectionChanged=(action externalAction)}}`);
-  this.$('.duke-ds-user-fullName').eq(3).click(); //click Jim row
+  this.$('.duke-ds-user-fullName').eq(2).click(); //click Jim row
 });
