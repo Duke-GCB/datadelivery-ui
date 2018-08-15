8d4c54e3256554f9924e8ee458ad7e27739b4186
John Bradley <johnbradley2008@gmail.com>
2018-08-15 15:26:17 -0400

    Fetch permissions with ajax response
    Replaces previous code with an observer that fetches
    permissions via ajax request.

---
+++ app/adapters/duke-ds-project.js
@@ -0,0 +1,15 @@
+import ApplicationAdapter from './application';
+
+export default ApplicationAdapter.extend({
+  getUserProjectAuthRole(projectId, userId) {
+    const dukeDsProjectUrl = this.buildURL('duke-ds-project', projectId);
+    const url = `${dukeDsProjectUrl}permissions/?user=${userId}`;
+    return this.ajax(url).then(response => {
+      const permissions = response['duke-ds-project-permissions'];
+      if (permissions) {
+        return permissions[0]['auth_role'];
+      }
+      return  null;
+    });
+  },
+});
--- tests/unit/controllers/deliveries/new/select-project-test.js
+++ tests/unit/controllers/deliveries/new/select-project-test.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 
 moduleFor('controller:deliveries/new/select-project', 'Unit | Controller | deliveries/new/select project', {
   // Specify the other units that are required for this test.
-  needs: ['controller:application', 'service:session', 'service:duke-ds-user', 'model:duke-ds-project-permission']
+  needs: ['controller:application', 'service:session', 'service:duke-ds-user']
 });
 
 // Replace this with your real tests.
@@ -23,7 +23,12 @@ test('it handles back action', function(assert) {
 });
 
 test('it handles next action', function(assert) {
-  let project = Ember.Object.create({ id: '123' });
+  let project = Ember.Object.create({
+    id: '123',
+    getUserProjectAuthRole() {
+      return Ember.RSVP.resolve('');
+    }
+  });
   let controller = this.subject({
     project: project,
     currentUserCanDeliver: true,
@@ -37,7 +42,12 @@ test('it handles next action', function(assert) {
 
 test('it stops next action if user cannot deliver', function(assert) {
   assert.expect(0);
-  let project = Ember.Object.create({ id: '123' });
+  let project = Ember.Object.create({
+    id: '123',
+    getUserProjectAuthRole() {
+      return Ember.RSVP.resolve('');
+    }
+  });
   let controller = this.subject({
     project: project,
     currentUserCanDeliver: false,
@@ -49,7 +59,12 @@ test('it stops next action if user cannot deliver', function(assert) {
 });
 
 test('it handles projectSelectionChanged', function(assert) {
-  let project = Ember.Object.create({ id: '123' });
+  let project = Ember.Object.create({
+    id: '123',
+    getUserProjectAuthRole() {
+      return Ember.RSVP.resolve('');
+    }
+  });
   let controller = this.subject({
     project: null,
   });
@@ -61,27 +76,35 @@ test('it handles projectSelectionChanged', function(assert) {
   assert.equal(controller.get('project'), project);
 });
 
-test('it computes currentUserProjectPermission from duke-ds-project-permission', function(assert) {
-  let project = Ember.Object.create({ id: '123' });
+test('it computes currentUserProjectAuthRole from getUserProjectAuthRole', function(assert) {
+  let project = Ember.Object.create({
+    id: '123',
+    getUserProjectAuthRole() {
+      return Ember.RSVP.resolve('project_admin');
+    }
+  });
   let currentDukeDsUser = Ember.Object.create({id: '456'});
-  const permission = Ember.Object.create({id: '789'});
   let controller = this.subject({
     project: null,
     currentDukeDsUser: null,
-    store: {
-      query: function () {
-        return Ember.RSVP.resolve(Ember.A([permission]));
-      }
-    }
   });
   assert.equal(controller.get('currentUserProjectPermission.authRole'), null);
-  controller.set('project', project);
-  controller.set('currentDukeDsUser', currentDukeDsUser);
-  controller.get('currentUserProjectPermission').then(permission => assert.equal(permission.id, '789'));
+  Ember.run(() => {
+    controller.setProperties({
+      project: project,
+      currentDukeDsUser: currentDukeDsUser
+    })
+  });
+  assert.equal(controller.get('currentUserProjectAuthRole'), 'project_admin');
 });
 
-test('it computes showUserMissingPrivilegesError from project and currentUserProjectPermission', function(assert) {
-  let project = Ember.Object.create({ id: '123' });
+test('it computes showUserMissingPrivilegesError from project and currentUserProjectAuthRole', function(assert) {
+  let project = Ember.Object.create({
+    id: '123',
+    getUserProjectAuthRole() {
+      return Ember.RSVP.resolve('');
+    },
+  });
   let controller = this.subject({
     project: null
   });
@@ -92,11 +115,11 @@ test('it computes showUserMissingPrivilegesError from project and currentUserPro
   assert.equal(controller.get('showUserMissingPrivilegesError'), false,
     'hide error when currentUserProjectAuthRole is empty');
 
-  controller.set('currentUserProjectPermission', Ember.Object.create({ authRole: 'downloader'}));
+  controller.set('currentUserProjectAuthRole', 'downloader');
   assert.equal(controller.get('showUserMissingPrivilegesError'), true,
     'show error when currentUserProjectAuthRole is not project admin');
 
-  controller.set('currentUserProjectPermission', Ember.Object.create({ authRole: 'project_admin'}));
+  controller.set('currentUserProjectAuthRole', 'project_admin');
   assert.equal(controller.get('showUserMissingPrivilegesError'), false,
     'hide error when currentUserProjectAuthRole is project admin');
 });
