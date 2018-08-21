da6e21276dbe276b5f8bb617bffc1863e94814e5
Dan Leehr <dan.leehr@duke.edu>
2018-08-20 21:58:05 -0400

    WIP on tests for confirm controller

--- tests/unit/controllers/deliveries/new/confirm-test.js
+++ tests/unit/controllers/deliveries/new/confirm-test.js
@@ -1,12 +1,103 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from "ember";
 
 moduleFor('controller:deliveries/new/confirm', 'Unit | Controller | deliveries/new/confirm', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  needs: ['controller:application', 'service:duke-ds-user', 'service:session']
 });
 
 // Replace this with your real tests.
 test('it exists', function(assert) {
   let controller = this.subject();
   assert.ok(controller);
+});
+
+test('it handles saveAndSend action', function(assert) {
+  assert.expect(7);
+  const project = Ember.Object.create({ id: '123' });
+  const fromUser = Ember.Object.create({ id: '222' });
+  const toUser = Ember.Object.create({ id: '456' });
+  const controller = this.subject({
+    generatePreview() {
+      // Block other calls to createRecord
+    },
+    projectId: '123',
+    toUserId: '456',
+    userMessage: 'hey bob',
+    currentDukeDsUser: fromUser,
+    store: {
+      findRecord(modelName, id) {
+        return Ember.RSVP.resolve(Ember.Object.create({id: id}));
+      },
+      createRecord(modelName, payload) {
+        assert.equal(modelName, 'delivery');
+        assert.deepEqual(payload.project, project);
+        assert.deepEqual(payload.fromUser, fromUser);
+        assert.deepEqual(payload.toUser, toUser);
+        assert.equal(payload.userMessage, 'hey bob');
+        const mockDelivery = {
+          get(name) {
+            if (name === 'transfer') {
+              return 'sometransferid';
+            }
+            return null;
+          }
+        };
+        mockDelivery.save = function () {
+          return Ember.RSVP.resolve(mockDelivery);
+        };
+        mockDelivery.send = function () {
+          return Ember.RSVP.resolve(mockDelivery);
+        };
+        return mockDelivery;
+      }
+    },
+    transitionToRoute(routeName, data) {
+      assert.equal(routeName, 'deliveries.show', 'next action should transition to show new delivery');
+      assert.equal(data, 'sometransferid', 'next action should pass project_id');
+    }
+  });
+  Ember.run(() => {
+    controller.send('saveAndSend');
+  });
+});
+
+test('it handles back action', function(assert) {
+  assert.ok(false, 'Not yet implemented');
+});
+
+test('it generates a preview when all required properties set', function(assert) {
+  const projectId = '123';
+  const fromUser = Ember.Object.create({id: '222'});
+  const toUserId = '456';
+  const userMessage = 'thanks';
+  const mockDelivery = Ember.Object.create({
+    preview(details) {
+      assert.deepEqual(details, {
+        project_id: projectId,
+        from_user_id: fromUser.get('id'),
+        to_user_id: toUserId,
+        user_message: userMessage,
+        transfer_id: '',
+      });
+      return Ember.RSVP.resolve({delivery_email_text: `Subject: ${details.user_message}`});
+    }
+  });
+  const controller = this.subject({
+    fromUser: fromUser,
+    store: {
+      createRecord(modelName) {
+        assert.equal(modelName, 'delivery');
+        return mockDelivery;
+      }
+    }
+  });
+  Ember.run(() => {
+    assert.notOk(controller.get('emailMessage'));
+    controller.set('projectId', projectId);
+    assert.notOk(controller.get('emailMessage'));
+    controller.set('toUserId', toUserId);
+    assert.notOk(controller.get('emailMessage'));
+    controller.set('userMessage', userMessage);
+  });
+  assert.equal(controller.get('emailMessage'), 'Subject: thanks');
 });
