1a74e30d0188cd80585cd24214388d8880cde65c
Dan Leehr <dan.leehr@duke.edu>
2018-08-29 15:14:51 -0400

    Fix issue where dev login did not redirect to deliveries page

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,3 +1,9 @@
 import LoginController from 'drf-ember-frontend/controllers/login';
 
-export default LoginController.extend();
+export default LoginController.extend({
+  authenticatedChanged: Ember.on('init', Ember.observer('session.isAuthenticated', function() {
+    if(this.get('session.isAuthenticated')) {
+      this.transitionToRoute('index');
+    }
+  }))
+});
