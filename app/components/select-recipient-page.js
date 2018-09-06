ace9555af8cf735478646c9fa960a686e9a03075
John Bradley <johnbradley2008@gmail.com>
2018-09-06 11:53:24 -0400

    revert select-recipient loading

--- app/components/select-recipient-page.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-  projectName: null, // string property - name of the project to display
-  backRoute: null, // string property - name of Back button destination
-  disableBack: null, // boolean property - disables Back button
-  nextRoute: null, // string property - name of Next button destination
-  disableNext: null, // boolean property - disables Next button
-});
