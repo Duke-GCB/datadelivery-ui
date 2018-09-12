62ac4cfc2483981b8557aec8fc216e03af47259a
John Bradley <johnbradley2008@gmail.com>
2018-09-12 14:10:27 -0400

    remove unused canRecall property

--- app/controllers/deliveries/show/index.js
+++ app/controllers/deliveries/show/index.js
@@ -1,8 +1,6 @@
 import CanResendController from './can-resend-controller';
-import Ember from "ember";
 
 export default CanResendController.extend({
   queryParams: ['infoMessage'],
-  infoMessage: null,
-  canRecall: Ember.computed.alias('canResend')
+  infoMessage: null
 });
