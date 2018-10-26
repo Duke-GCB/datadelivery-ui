8a7ba5134fd4801b647f85373552a411e29044bf
Dan Leehr <dan.leehr@duke.edu>
2018-10-26 15:01:05 -0400

    Figuring out markup for user search form

--- app/components/duke-ds-user-search-fields.js
+++ app/components/duke-ds-user-search-fields.js
@@ -1,8 +1,17 @@
 import Ember from 'ember';
 
+const MODES = [
+  {mode: 'name', name: 'Name'},
+  {mode: 'username', name: 'NetID'},
+  {mode: 'email', name: 'Email'}
+];
+
+const DEFAULT_MODE = MODES[0];
+
 export default Ember.Component.extend({
   query: null,
-  mode: 'username',
+  modes: MODES,
+  mode: null,
   onSearch: () => {}, // Default implementation
   actions: {
     search() {
@@ -10,5 +19,10 @@ export default Ember.Component.extend({
       const mode = this.get('mode');
       this.get('onSearch')(mode, query);
     }
+  },
+  didReceiveAttrs() {
+    this._super(...arguments);
+    this.set('mode', DEFAULT_MODE.mode);
+
   }
 });
--- app/components/duke-ds-user-search.js
+++ app/components/duke-ds-user-search.js
@@ -4,16 +4,14 @@ export default Ember.Component.extend({
   classNames: ['duke-ds-user-search'],
   users: null,
   actions: {
-    doSearch(args) {
-      Ember.Logger.log(`Do the search ${args}`);
-      this.get('users').addObject(args);
+    doSearch(mode, query) {
+      Ember.Logger.log();
+      this.get('users').addObject(`${mode} ${query}`);
     }
   },
 
   didReceiveAttrs() {
     this._super(...arguments);
     this.set('users', []);
-
   }
-
 });
