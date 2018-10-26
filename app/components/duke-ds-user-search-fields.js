48caa08f26a573f759e1eccc2f906521d5e43673
Dan Leehr <dan.leehr@duke.edu>
2018-10-26 15:22:57 -0400

    Continuing to build up search markup

--- app/components/duke-ds-user-search-fields.js
+++ app/components/duke-ds-user-search-fields.js
@@ -1,28 +1,28 @@
 import Ember from 'ember';
 
-const MODES = [
-  {mode: 'name', name: 'Name'},
-  {mode: 'username', name: 'NetID'},
-  {mode: 'email', name: 'Email'}
+const KINDS = [
+  {mode: 'name', name: 'Name', placeholder: 'Person\'s Name'},
+  {mode: 'username', name: 'NetID', placeholder: 'Duke NetID'},
+  {mode: 'email', name: 'Email', placeholder: 'Email Address'}
 ];
 
-const DEFAULT_MODE = MODES[0];
+const DEFAULT_KIND = KINDS[0];
 
 export default Ember.Component.extend({
   query: null,
-  modes: MODES,
-  mode: null,
+  kinds: KINDS,
+  kind: null,
   onSearch: () => {}, // Default implementation
   actions: {
     search() {
       const query = this.get('query');
-      const mode = this.get('mode');
+      const mode = this.get('kind.mode');
       this.get('onSearch')(mode, query);
     }
   },
   didReceiveAttrs() {
     this._super(...arguments);
-    this.set('mode', DEFAULT_MODE.mode);
+    this.set('kind', DEFAULT_KIND);
 
   }
 });
