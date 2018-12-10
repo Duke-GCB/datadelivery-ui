d6b61175ef67f3dcf365e74feae6cad708d70049
Dan Leehr <dan.leehr@duke.edu>
2018-12-10 14:27:29 -0500

    Remove unused import and leftover comments

--- tests/unit/routes/deliveries/new/select-recipient-test.js
+++ tests/unit/routes/deliveries/new/select-recipient-test.js
@@ -1,9 +1,6 @@
 import { moduleFor, test } from 'ember-qunit';
-import Ember from 'ember';
 
 moduleFor('route:deliveries/new/select-recipient', 'Unit | Route | deliveries/new/select recipient', {
-  // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
 });
 
 test('it exists', function(assert) {
