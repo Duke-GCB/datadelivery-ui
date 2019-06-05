d8b76f28b679863d5ecb7d1d82e3fd18cb561434
Dan Leehr <dan.leehr@duke.edu>
2019-06-05 14:10:59 -0400

    Move showProjectDetails computed property to deliveries/show/index
    This property is not relevant to all descendants of can-resend-controller, so I'm moving it to the appropriate location

--- app/controllers/deliveries/show/can-resend-controller.js
+++ app/controllers/deliveries/show/can-resend-controller.js
@@ -14,22 +14,5 @@ export default Ember.Controller.extend({
       return false;
     }
     return modelCanResend && fromUserId == currentDukeDsUserId;
-  }),
-  showProjectDetails: Ember.computed('model.status', 'model.fromUser.id', 'currentDukeDsUser.id', function () {
-    // Show the details if the currentDukeDsUser is the sender or if they're the recipient and they've received it
-    const currentDukeDsUserId = this.get('currentDukeDsUser.id');
-    if (!currentDukeDsUserId) {
-      // Cannot determine current user, assume we should not show
-      return false;
-    }
-    const fromUserId = this.get('model.fromUser.id');
-    if (fromUserId === currentDukeDsUserId) {
-      // Delivery sender is the current user, assume they'll have download access
-      return true;
-    } else {
-      // User must be a recipient if they have access to the transfer and are not the sender
-      // Only show the details if they've accepted the project and would have access
-      return this.get('model.status') === 'accepted';
-    }
   })
 });
