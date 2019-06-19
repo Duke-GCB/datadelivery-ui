f60c2cf474ede9fccc0dea1faae9829ebec09a25
Dan Leehr <dan.leehr@duke.edu>
2019-06-19 15:56:44 -0400

    Begin rewriting delivery-breadcrumbs-test

--- tests/integration/components/delivery-breadcrumbs-test.js
+++ tests/integration/components/delivery-breadcrumbs-test.js
@@ -6,44 +6,11 @@ moduleForComponent('delivery-breadcrumbs', 'Integration | Component | delivery b
   integration: true
 });
 
-test('it renders Data Delivery when deliveries.index selected', function(assert) {
-  this.render(hbs`{{delivery-breadcrumbs selectedRouteName="deliveries.index"}}`);
-  assert.equal(this.$('li').text().trim(), 'Data Delivery');
-  assert.equal(this.$('a').length, 0, 'contains no anchors');
-});
-
-test('it renders with delivery.show selected', function(assert) {
-  const transfer = Ember.Object.create({
-    project: {
-      name: 'Some Project'
-    }
-  });
-  this.set('transfer', transfer);
-  this.render(hbs`{{delivery-breadcrumbs selectedRouteName="deliveries.show" transfer=transfer}}`);
-  assert.equal(this.$('a').length, 1, 'there is one anchor');
-
-  assert.equal(this.$('a').text().trim(), 'Data Delivery', 'Data Delivery is a link');
-  assert.equal(this.$('li').eq(1).text().trim(), 'Some Project', 'displays project name as text');
-});
-
-test('it renders with delivery.resend selected', function(assert) {
-  const transfer = Ember.Object.create({
-    project: {
-      name: 'Some Project'
-    }
-  });
-  this.set('transfer', transfer);
-  this.render(hbs`{{delivery-breadcrumbs selectedRouteName="deliveries.show.resend" transfer=transfer}}`);
-  assert.equal(this.$('a').eq(0).text().trim(), 'Data Delivery');
-  assert.equal(this.$('a').eq(1).text().trim(), 'Some Project');
-  assert.equal(this.$('a').length, 2, 'There are two links to parent routes.');
-  assert.equal(this.$('li').eq(2).text().trim(), 'resend', 'show resend as text');
-});
-
-test('it renders with delivery.new selected', function(assert) {
-  this.render(hbs`{{delivery-breadcrumbs selectedRouteName="deliveries.new"}}`);
-  assert.equal(this.$('li').eq(0).text().trim(), 'Data Delivery');
-  assert.equal(this.$('li').eq(1).text().trim(), 'New');
-  assert.equal(this.$('a').length, 1, 'there is one anchor');
-  assert.equal(this.$('a').text().trim(), 'Data Delivery', 'Data Delivery is a link');
+test('it renders an li for each crumb and an a for the non-selected routes', function(assert) {
+  this.render(hbs`{{delivery-breadcrumbs currentRouteName='deliveries'}}`);
+  assert.equal(this.$('li').length, 2);
+  assert.equal(this.$('li:eq(0)').text().trim(), 'Home');
+  assert.equal(this.$('li:eq(1)').text().trim(), 'Deliveries');
+  assert.equal(this.$('a').length, 1);
+  assert.equal(this.$('a').text().trim(), 'Home');
 });
