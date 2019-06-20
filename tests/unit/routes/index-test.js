ceacf81b38c0e710a7395c309e95f4996f53c3f8
Dan Leehr <dan.leehr@duke.edu>
2019-06-20 09:32:34 -0400

    Remove redirect test for index route
    - This route is no longer intended to redirect, so the test is wrong

--- tests/unit/routes/index-test.js
+++ tests/unit/routes/index-test.js
@@ -7,12 +7,3 @@ test('it exists', function(assert) {
   let route = this.subject();
   assert.ok(route);
 });
-
-test('it redirects to deliveries', function(assert) {
-  let route = this.subject({
-    replaceWith(routeName) {
-      assert.equal(routeName, 'deliveries','replace with route name deliveries');
-    }
-  });
-  route.beforeModel();
-});
