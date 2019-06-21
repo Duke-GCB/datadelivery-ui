f18b6cce542c48859bd813d15efec3f30ccec5ed
John Bradley <johnbradley2008@gmail.com>
2019-06-21 12:21:59 -0400

    fix multi-line string tests in delivery-email

--- tests/integration/components/delivery-email-test.js
+++ tests/integration/components/delivery-email-test.js
@@ -10,30 +10,30 @@ module('Integration | Component | delivery email', function(hooks) {
 
     await render(hbs`{{delivery-email 'Subject: Lunch
     
-  data'}}`);
+data'}}`);
     assert.equal(find('.delivery-email').innerHTML.trim(), 'Subject: Lunch<br><br>data<br>');
   });
 
   test('it renders four header fields and body', async function(assert) {
 
     await render(hbs`{{delivery-email 'Subject: Lunch
-  From: joe@joe.joe
-  To: bob@bob.bob
-  Date: Jan 2 2019 12:30
-    
-  Hey,
-  When do you want to eat lunch?
-  -joe'}}`);
+From: joe@joe.joe
+To: bob@bob.bob
+Date: Jan 2 2019 12:30
+  
+Hey,
+When do you want to eat lunch?
+-joe'}}`);
     assert.equal(find('.delivery-email').innerHTML.trim(), `Subject: Lunch<br>Date: Jan 2 2019 12:30<br><br>Hey,<br>When do you want to eat lunch?<br>-joe<br>`);
   });
 
   test('it skips unknown header lines', async function(assert) {
     await render(hbs`{{delivery-email 'Subject: Eat Lunch
-  Junk: Lunch
+Junk: Lunch
 
-  Hey,
-  When do you want to eat lunch?
-  -joe'}}`);
+Hey,
+When do you want to eat lunch?
+-joe'}}`);
     assert.equal(find('.delivery-email').innerHTML.trim(), `Subject: Eat Lunch<br><br>Hey,<br>When do you want to eat lunch?<br>-joe<br>`)
   });
 });
