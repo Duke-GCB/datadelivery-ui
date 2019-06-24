d1890f45c8b43f1c766aa82b44b155f3c4d3f000
John Bradley <johnbradley2008@gmail.com>
2019-06-24 09:58:35 -0400

    fix duke-ds-user-search-fields-test by using click helper function

--- tests/integration/components/duke-ds-user-search-fields-test.js
+++ tests/integration/components/duke-ds-user-search-fields-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render, find, click, fillIn, triggerEvent } from '@ember/test-helpers';
+import { render, find, click } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | duke ds user search fields', function(hooks) {
@@ -14,54 +14,31 @@ module('Integration | Component | duke ds user search fields', function(hooks) {
     assert.equal(this.$('label.mode').eq(2).text().trim(), 'Email');
   });
 
-  test('it calls onSearch with name modes', async function(assert) {
-    let testParam = {query: 'Jane Doe', mode: 'full_name_contains', placeholder: 'Full or Partial Name'};
-    const onSearch = function(searchParam) {
-      assert.deepEqual(searchParam[testParam.mode], testParam.query);
-    };
-    this.set('onSearch', onSearch);
-    await render(hbs`{{duke-ds-user-search-fields onSearch=onSearch}}`);
-    await fillIn('input.query-field', testParam.query);
-    await triggerEvent('input.query-field', 'change'); // change() is required to trigger the update
-    // Click the mode's radio button
-    this.$('input[type=radio].' + testParam.mode).click();
-    // Verify the placeholder is updated
-    assert.equal(find('input.query-field').getAttribute('placeholder'), testParam.placeholder);
-    // Click Search
-    await click('button');
-  });
+  test('it calls onSearch with name, username, and email modes', async function(assert) {
+    const modesAndQueries = [
+      {query: 'Jane Doe', mode: 'full_name_contains', placeholder: 'Full or Partial Name'},
+      {query: 'jdoe123', mode: 'username', placeholder: 'Exact Duke NetID'},
+      {query: 'jane.doe@example.org', mode: 'email', placeholder: 'Exact Email Address'}
+    ];
 
-  test('it calls onSearch with username mode', async function(assert) {
-    let testParam = {query: 'jdoe123', mode: 'username', placeholder: 'Exact Duke NetID'};
-    const onSearch = function(searchParam) {
-      assert.deepEqual(searchParam[testParam.mode], testParam.query);
-    };
-    this.set('onSearch', onSearch);
-    await render(hbs`{{duke-ds-user-search-fields onSearch=onSearch}}`);
-    await fillIn('input.query-field', testParam.query);
-    await triggerEvent('input.query-field', 'change'); // change() is required to trigger the update
-    // Click the mode's radio button
-    this.$('input[type=radio].' + testParam.mode).click();
-    // Verify the placeholder is updated
-    assert.equal(find('input.query-field').getAttribute('placeholder'), testParam.placeholder);
-    // Click Search
-    await click('button');
+    assert.expect(modesAndQueries.length * 2); // two assertions for every mode
+    for (var i = 0; i < modesAndQueries.length; i++) {
+      const testParam = modesAndQueries[i];
+      const onSearch = function (searchParam) {
+        assert.deepEqual(searchParam[testParam.mode], testParam.query);
+      };
+      this.set('onSearch', onSearch);
+      await this.render(hbs`{{duke-ds-user-search-fields onSearch=onSearch}}`);
+      await this.$('input.query-field').val(testParam.query);
+      await this.$('input.query-field').change(); // change() is required to trigger the update
+      // Click the mode's radio button
+      //await this.$('input[type=radio].' + testParam.mode).click();
+      await click('input[type=radio].' + testParam.mode);
+      // Verify the placeholder is updated
+      assert.equal(this.$('input.query-field').attr('placeholder'), testParam.placeholder);
+      // Click Search
+      this.$('button').click();
+    }
   });
-
-  test('it calls onSearch with email mode', async function(assert) {
-    let testParam = {query: 'jane.doe@example.org', mode: 'email', placeholder: 'Exact Email Address'};
-    const onSearch = function(searchParam) {
-      assert.deepEqual(searchParam[testParam.mode], testParam.query);
-    };
-    this.set('onSearch', onSearch);
-    await render(hbs`{{duke-ds-user-search-fields onSearch=onSearch}}`);
-    await fillIn('input.query-field', testParam.query);
-    await triggerEvent('input.query-field', 'change'); // change() is required to trigger the update
-    // Click the mode's radio button
-    this.$('input[type=radio].' + testParam.mode).click();
-    // Verify the placeholder is updated
-    assert.equal(find('input.query-field').getAttribute('placeholder'), testParam.placeholder);
-    // Click Search
-    await click('button');
-  });
+  ///});
 });
