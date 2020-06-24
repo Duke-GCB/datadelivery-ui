4f2900350be5a7c95475ff9fe3709be3ce49d6db
John Bradley <johnbradley2008@gmail.com>
2020-06-24 16:39:24 -0400

    additional email-template-set tests

--- tests/unit/models/email-template-test.js
+++ tests/unit/models/email-template-test.js
@@ -10,4 +10,23 @@ module('Unit | Model | email template', function(hooks) {
     let model = store.createRecord('email-template', {});
     assert.ok(model);
   });
+
+  test('it computes content from subject and body', function(assert) {
+    let store = this.owner.lookup('service:store');
+    let model = store.createRecord('email-template', {
+      body: 'mybody',
+      subject: 'subject',
+    });
+    assert.equal(model.get('content').trim(), 'Subject: subject\n\nmybody');
+  });
+
+  test('it computes userType from type', function(assert) {
+    let store = this.owner.lookup('service:store');
+    let model = store.createRecord('email-template', {
+      type: 'one_two_three',
+      body: 'mybody',
+      subject: 'subject',
+    });
+    assert.equal(model.get('userType').trim(), 'One Two Three');
+  });
 });
--- tests/unit/routes/deliveries/new-test.js
+++ tests/unit/routes/deliveries/new-test.js
@@ -1,5 +1,7 @@
+import EmberObject from '@ember/object';
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import { resolve } from 'rsvp';
 
 module('Unit | Route | deliveries/new', function(hooks) {
   setupTest(hooks);
@@ -7,5 +9,24 @@ module('Unit | Route | deliveries/new', function(hooks) {
   test('it exists', function(assert) {
     let route = this.owner.lookup('route:deliveries/new');
     assert.ok(route);
+  });
+
+  test('it populates default email-template-set', async function(assert) {
+    assert.expect(4);
+    let route = this.owner.factoryFor('route:deliveries/new').create({
+      store: {
+        query(recordModel, query_param) {
+          assert.equal(recordModel, 'email-template-set');
+          assert.deepEqual(query_param, { filter: {default: true}});
+          return resolve([EmberObject.create({id:4})]);
+        },
+        createRecord(recordModel) {
+          assert.equal(recordModel, 'delivery');
+          return EmberObject.create({id:1});
+        }
+      }
+    });
+    const model = await route.model();
+    assert.equal(model.get('emailTemplateSet.id'), 4);
   });
 });
