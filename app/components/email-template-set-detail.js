cbe0d3fc56bb28b758c1361c991d3e646710cfbe
John Bradley <johnbradley2008@gmail.com>
2020-06-26 14:30:32 -0400

    add eof newlines and remove unused code

--- app/components/email-template-set-detail.js
+++ app/components/email-template-set-detail.js
@@ -1,13 +1,4 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  init() {
-    this._super(...arguments);
-    this.columns = [
-      {propertyName: "name", title: "Name",  routeName: "email-template-sets.show"},
-      {propertyName: "ccAddress", title: "CC Address"},
-      {propertyName: "replyAddress", title: "Reply-To Address"},
-      {propertyName: "default", title: "Default Template"},
-    ];
-  }
 });
