c5c2e4f093505f4e4e95637416cef8fb35427791
John Bradley <johnbradley2008@gmail.com>
2020-06-29 16:59:53 -0400

    improve testing for email-template-set-detail

--- tests/integration/components/email-template-set-detail-test.js
+++ tests/integration/components/email-template-set-detail-test.js
@@ -1,14 +1,25 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import EmberObject from '@ember/object';
 
 module('Integration | Component | email-template-set-detail', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    await render(hbs`{{email-template-set-detail}}`);
+    const model = EmberObject.create({
+      emailTemplates: [EmberObject.create({
+        userType: 'delivery',
+        helpText: 'help info',
+        content: 'Testing\nTesting\nTesting'
+      })]
+    });
+    this.set('model', model);
 
-    assert.equal(this.element.textContent.trim(), '');
+    await render(hbs`{{email-template-set-detail model=model}}`);
+
+    assert.equal(find('.email-template-detail-type').innerHTML, 'delivery - Email Template');
+    assert.equal(find('.email-template-detail-help').innerHTML, 'help info');
   });
 });
