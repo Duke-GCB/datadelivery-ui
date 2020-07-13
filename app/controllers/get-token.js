c5aba127b92ef76504fda7aead9980390fbfea40
John Bradley <johnbradley2008@gmail.com>
2020-07-13 13:35:33 -0400

    test successRoute override from login

--- app/controllers/get-token.js
+++ app/controllers/get-token.js
@@ -1,6 +1,7 @@
 import GetTokenController from 'drf-ember-frontend/controllers/get-token';
 
 export default GetTokenController.extend({
+  queryParams: ['successRoute'],
   successRoute: '/',
   failureRoute: '/login',
 });
