5a7326eabe05fbd9c64c2552a5ef0543d531ca3f
John Bradley <johnbradley2008@gmail.com>
2020-07-14 09:03:35 -0400

    redirect to intended destination after login

--- app/routes/email-template-sets.js
+++ app/routes/email-template-sets.js
@@ -1,15 +1,8 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Route.extend(AuthenticatedRouteMixin, {
-  router: service(),
   triggerAuthentication() {
-    let authenticationRoute = this.get('authenticationRoute');
-    this.transitionTo(authenticationRoute, {
-      queryParams: {
-        sort: this.get('router.currentRouteName')
-      }
-    });
-  },
+    this.transitionTo(this.get('authenticationRoute'), { queryParams: { successRoute: 'email-template-sets'}});
+  }
 });
