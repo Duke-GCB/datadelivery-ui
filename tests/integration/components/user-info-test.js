6b85404ae284acda08774ca1c5175112021314fb
John Bradley <johnbradley2008@gmail.com>
2020-08-21 14:23:31 -0400

    create test for user-info

--- tests/integration/components/user-info-test.js
+++ tests/integration/components/user-info-test.js
@@ -2,25 +2,25 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import EmberObject from '@ember/object';
+
 
 module('Integration | Component | user-info', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`{{user-info}}`);
-
+  test('it is blank when user is null', async function(assert) {
+    this.set('user', null);
+    await render(hbs`{{user-info user=user}}`);
     assert.equal(this.element.textContent.trim(), '');
+  });
 
-    // Template block usage:
-    await render(hbs`
-      {{#user-info}}
-        template block text
-      {{/user-info}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+  test('it is blank when user is null', async function(assert) {
+    this.set('user', EmberObject.create({
+      first_name: "Joe",
+      last_name: "Smith",
+      username: "joe",
+    }));
+    await render(hbs`{{user-info user=user}}`);
+    assert.equal(this.element.textContent.trim(), "Logged in as Joe Smith (joe)");
   });
 });
