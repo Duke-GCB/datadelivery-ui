7bf6c32ed34147a334348fbcec58284bb30f8b1d
John Bradley <johnbradley2008@gmail.com>
2021-01-15 11:02:34 -0500

    upgrade to ember 3.20

--- tests/integration/components/project-created-date-test.js
+++ tests/integration/components/project-created-date-test.js
@@ -1,16 +1,19 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
 import EmberObject from '@ember/object';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('project-created-date', 'Integration | Component | project created date', {
-  integration: true
-});
+module('Integration | Component | project created date', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders', function(assert) {
-  const record = EmberObject.create({
-    createdOn: '2020-01-01'
+  test('it renders', async function(assert) {
+    const record = EmberObject.create({
+      createdOn: '2020-01-01'
+    });
+    this.set('record', record);
+    await render(hbs`{{project-created-date record=record}}`);
+    assert.equal(this.$().text().trim(), 'January 1, 2020 12:00 AM');
   });
-  this.set('record', record);
-  this.render(hbs`{{project-created-date record=record}}`);
-  assert.equal(this.$().text().trim(), 'January 1, 2020 12:00 AM');
+
 });
