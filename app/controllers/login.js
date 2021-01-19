3c54304e5b9fdc82b69856d4db8440d106cb65f5
John Bradley <johnbradley2008@gmail.com>
2021-01-19 11:10:25 -0500

    fix redirect after login
    Removes un-needed transitionToRoute that already handled by
    GetTokenController.

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,13 +1,7 @@
 import { observer } from '@ember/object';
-import { on } from '@ember/object/evented';
 import LoginController from 'drf-ember-frontend/controllers/login';
 
 export default LoginController.extend({
   queryParams: ['afterLogin'],
-  afterLogin: '/',
-  authenticatedChanged: on('init', observer('session.isAuthenticated', function() {
-    if(this.get('session.isAuthenticated')) {
-      this.transitionToRoute('index');
-    }
-  }))
+  afterLogin: '/'
 });
